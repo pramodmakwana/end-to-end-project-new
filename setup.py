1d6bf50c253cebef5a3dd8f946094feba4afd80c
pramod1117789@gmail.com <pramod1117789@gmail.com>
2023-11-08 22:08:06 +0530

    updated few files

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
 
-from setuptools import find_packgaes,setup    
-    """HYPEN_E_DOT='-e .'
-    
-    def get_requirements(file_path:str)->List[str]:
-        requirements=[]
-        with open(file_path) as file_obj:
-         requirements=file_obj.readlines(
-         requirements=[req.replace("n\","")for req in requirements]
-         
-         if HYPEN_E_DOT in requirements:
-             requirements.remove(HYPEN_E_DOT)
-             
-        return requirements"""
-        
-    
-    
-    
+"""HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements"""
+
 setup(
-    name='DiamonPricePrediction',
+    name='DimondPricePrediction',
     version='0.0.1',
     author='pramod makwana',
     author_email='pramod1117789@gmail.com',
-    packages=find_packages()  
+    install_requires=["scikit-learn","pandas","numpy"],
+    packages=find_packages()
     
 )
